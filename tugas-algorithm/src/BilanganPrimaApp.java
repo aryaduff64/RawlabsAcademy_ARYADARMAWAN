69a51335d1950b4a8b706e7f498f6b91bba8d031
Arya Darmawan <arya.darmawan5800@gmail.com>
2023-01-29 16:22:26 +0700

    update

--- tugas-algorithm/src/BilanganPrimaApp.java
+++ tugas-algorithm/src/BilanganPrimaApp.java
@@ -25,16 +25,16 @@ public class BilanganPrimaApp {
         m = n / 2;
 
         if (n == 0 || n == 1) {
-            result = "is not prime number";
+            result = "is not prime numbers";
         } else {
             for (i = 2; i <= m; i++) {
                 if (n % i == 0) {
-                    result = "is not prime number";
+                    result = "is not prime numbers";
                     flag = 1;
                 }
             }
             if (flag == 0) {
-                result = "is prime number";
+                result = "is prime numbers";
             }
         }
         return result;
