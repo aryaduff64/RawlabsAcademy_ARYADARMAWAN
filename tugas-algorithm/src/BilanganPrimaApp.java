2432423f75d42b35b39903f9183ec052804d8d89
Arya Darmawan <arya.darmawan5800@gmail.com>
2023-01-29 15:55:17 +0700

    algorithm commit

---
+++ tugas-algorithm/src/BilanganPrimaApp.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class BilanganPrimaApp {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int ch;
+        do {
+            System.out.println("Silahkan masukan angka : ");
+            ch = s.nextInt();
+            String hasilnya = prosesBilanganPrima(ch);
+            System.out.println("hasil :" + " " + hasilnya);
+            break;
+        } while (ch != 0);
+
+    }
+
+    public static String prosesBilanganPrima(int nmbr) {
+        String result = null;
+
+        int i, m = 0, flag = 0;
+        int n = nmbr; //it is the number to be checked
+
+        m = n / 2;
+
+        if (n == 0 || n == 1) {
+            result = "is not prime number";
+        } else {
+            for (i = 2; i <= m; i++) {
+                if (n % i == 0) {
+                    result = "is not prime number";
+                    flag = 1;
+                }
+            }
+            if (flag == 0) {
+                result = "is prime number";
+            }
+        }
+        return result;
+    }
+}
+
